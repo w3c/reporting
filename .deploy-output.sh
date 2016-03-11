e8aa90482b0c4dc7c293da404d7e371b0848d837
plehegar <plh@w3.org>
2016-03-11 16:55:27 -0500

    Added automatic deployment on GitHub

---
+++ .deploy-output.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+STATUS=`git log -1 --pretty=oneline`
+
+cp single-page.html ./out/
+
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "travis-ci"
+ls
+git add .
+git commit -m "Built by Travis-CI: $STATUS"
+git status
+
+GH_REPO="@github.com/w3c/html.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
