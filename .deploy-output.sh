936d27163ec07e35ab38e4d94825b1fc6da3d757
Patrick Kettner <patrickkettner@gmail.com>
2017-02-07 20:06:33 -0800

    update travis script to fix publishing

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -14,6 +14,6 @@ git add .
 git commit -m "Built by Travis-CI: $STATUS"
 git status
 
-GH_REPO="@github.com/w3c/html.git"
+GH_REPO="@github.com/wicg/reporting.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
