1842cac3416c5259e1e9c4091b0e6a3e31f45dde
Ian Clelland <iclelland@chromium.org>
2020-01-09 11:44:40 -0500

    Add network reporting draft

--- compile.sh
+++ compile.sh
@@ -4,7 +4,12 @@ set -e # Exit with nonzero exit code if anything fails
 echo Running bikeshed on index.src.html
 bikeshed -f spec ./index.src.html
 
+echo Running bikeshed on network-reporting.bs
+bikeshed -f spec ./network-reporting.bs
+
 if [ -d out ]; then
   echo Copy index.html into out/index.html
   cp index.html out/index.html
+  echo Copy network-reporting.html into out/network-reporting.html
+  cp network-reporting.html out/network-reporting.html
 fi
