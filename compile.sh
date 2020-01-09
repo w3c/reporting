d44d281b34e754d50939b1ea1807c30209936115
Ian Clelland <iclelland@chromium.org>
2020-01-09 15:54:20 -0500

    Update deploy and compile scripts to support long-lived target branches

--- compile.sh
+++ compile.sh
@@ -4,7 +4,21 @@ set -e # Exit with nonzero exit code if anything fails
 echo Running bikeshed on index.src.html
 bikeshed -f spec ./index.src.html
 
-if [ -d out ]; then
-  echo Copy index.html into out/index.html
-  cp index.html out/index.html
+for SPEC in ./*.bs; do
+  echo Running bikeshed on $SPEC
+  bikeshed -f spec $SPEC
+done
+
+OUTDIR=${1:-out}
+
+if [ -d $OUTDIR ]; then
+  echo Copy index.html into $OUTDIR/index.html
+  cp index.html $OUTDIR/index.html
+  for SPEC in ./*.bs; do
+    SPEC_OUT=${SPEC%.bs}.html
+    if [ -f $SPEC_OUT ]; then
+      echo Copy $SPEC_OUT into $OUTDIR/$SPEC_OUT
+      cp $SPEC_OUT $OUTDIR/$SPEC_OUT
+    fi
+  done
 fi
