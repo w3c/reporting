70e11a1a245472533149c60e27c43c2a7b7b3c1f
Ian Clelland <iclelland@chromium.org>
2020-01-10 10:52:01 -0500

    Fix to work correctly when no .bs files are present

--- compile.sh
+++ compile.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
+shopt -q nullglob
+NULLGLOB_WAS_SET=$?
+shopt -s nullglob
+FILES=$(echo *.bs)
+if [ 1 -eq $NULLGLOB_WAS_SET ]; then
+  shopt -u nullglob
+fi
+
 set -e # Exit with nonzero exit code if anything fails
 
 echo Running bikeshed on index.src.html
 bikeshed -f spec ./index.src.html
 
-for SPEC in ./*.bs; do
+for SPEC in $FILES; do
   echo Running bikeshed on $SPEC
   bikeshed -f spec $SPEC
 done
@@ -14,7 +23,7 @@ OUTDIR=${1:-out}
 if [ -d $OUTDIR ]; then
   echo Copy index.html into $OUTDIR/index.html
   cp index.html $OUTDIR/index.html
-  for SPEC in ./*.bs; do
+  for SPEC in $FILES; do
     SPEC_OUT=${SPEC%.bs}.html
     if [ -f $SPEC_OUT ]; then
       echo Copy $SPEC_OUT into $OUTDIR/$SPEC_OUT
