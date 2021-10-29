0b5032c418b9aa778bb22a08358e228861bf9730
Ian Clelland <iclelland@chromium.org>
2021-10-29 11:11:55 -0400

    Update auto-publish scripts

--- compile.sh
+++ compile.sh
@@ -10,24 +10,37 @@ fi
 
 set -e # Exit with nonzero exit code if anything fails
 
-echo Running bikeshed on index.src.html
-bikeshed -f spec ./index.src.html
-
-for SPEC in $FILES; do
+for SPEC in index.src.html $FILES; do
   echo Running bikeshed on $SPEC
-  bikeshed -f spec $SPEC
+  if which bikeshed; then
+    bikeshed -f spec $SPEC
+  else
+    SPEC_OUT=${SPEC%.bs}.html
+    HTTP_STATUS=$(curl https://api.csswg.org/bikeshed/ \
+                       --output ${SPEC_OUT} \
+                       --write-out "%{http_code}" \
+                       --header "Accept: text/plain, text/html" \
+                       -F file=@${SPEC})
+    if [ "$HTTP_STATUS" -ne "200" ]; then
+      echo ""; cat $SPEC_OUT; echo ""
+      rm -f $SPEC_OUT
+      exit 1
+    fi
+  fi
 done
 
 OUTDIR=${1:-out}
 
+mkdir -p $OUTDIR
+
 if [ -d $OUTDIR ]; then
-  echo Copy index.html into $OUTDIR/index.html
-  cp index.html $OUTDIR/index.html
+  echo Move index.html into $OUTDIR/index.html
+  mv index.html $OUTDIR/$SPEC_OUT
   for SPEC in $FILES; do
     SPEC_OUT=${SPEC%.bs}.html
     if [ -f $SPEC_OUT ]; then
-      echo Copy $SPEC_OUT into $OUTDIR/$SPEC_OUT
-      cp $SPEC_OUT $OUTDIR/$SPEC_OUT
+      echo Move $SPEC_OUT into $OUTDIR/$SPEC_OUT
+      mv $SPEC_OUT $OUTDIR/$SPEC_OUT
     fi
   done
 fi
